7269f5824085033a9467f3ae99ca3863f8f23b94
Airstorn <mitichkinvladnov@gmail.com>
2020-04-02 15:51:52 +0700

    grid

---
+++ Assets/Scripts/Battle/Grid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class GridElement
+{
+    [SerializeField] private Vector2Int _id;
+    [SerializeField] private GridUnit _unit;
+    [SerializeField] private Vector3 _cellPos;
+
+    public Vector2Int Id => _id;
+    public GridUnit Unit => _unit;
+    public Vector3 CellPos => _cellPos;
+
+    public GridElement(Vector2Int id, Vector3 worldPos, GridUnit unit)
+    {
+        _id = id;
+        _unit = unit;
+        _cellPos = worldPos;
+    }
+}
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private GridElement[,] _objects = new GridElement[10, 10];
+    [SerializeField] private float _tileOffset = 1;
+    [SerializeField] private GameObject _cellTemplate;
+    [SerializeField] private UnitsData _data;
+
+    private void Start()
+    {
+        InitGrid();
+    }
+
+    private void InitGrid()
+    {
+        for (int x = 0; x < _objects.GetLength(0); x++)
+        {
+            for (int z = 0; z < _objects.GetLength(1); z++)
+            {
+                _objects[x, z] = new GridElement(
+                    new Vector2Int(x, z),
+                    new Vector3(x * _tileOffset,0, z * _tileOffset),
+                    _data.GetUnit<GridUnits.Empty>()
+                    );
+
+                var cell = Instantiate(_cellTemplate, _objects[x, z].CellPos, Quaternion.Euler(90,0,0));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Battle/UnitsData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[CreateAssetMenu(fileName = "New Data", menuName = "Battle/New Units Data")]
+public class UnitsData : ScriptableObject
+{
+    public GridUnit[] Data;
+
+    public GridUnit GetUnit<T>() where T : GridUnit
+    {
+        return Data.OfType<T>().First();
+    }
+}
