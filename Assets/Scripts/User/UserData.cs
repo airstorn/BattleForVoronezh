e154d4d5a7e9dfac030c161497d76cf18e113670
airstorn <mitichkinvladnov@gmail.com>
2020-04-21 21:21:28 +0700

    menu

--- Assets/Scripts/User/UserData.cs
+++ Assets/Scripts/User/UserData.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class UserData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+      [SerializeField] private string _username;
+      [SerializeField] private int _coins;
+      [SerializeField] private List<GridUnit> _units;
 }
